8aa06ea1e1bdd16955b0c3909917c18fb26a25d9
fajaramaulana <fajaragusmaulana@gmail.com>
2022-08-22 20:17:17 +0700

    assignment 2 22/08/2022

---
+++ assignmentslice.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	names := []string{"zayan", "billy", "rizky", "fajar", "zaka", "hendar", "wahyu", "tambunan", "ramadhan", "utomo"}
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
