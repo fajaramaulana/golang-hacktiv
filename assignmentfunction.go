4a26c3d1d22fe164c198186e28e386f593d82adb
fajaramaulana <fajaragusmaulana@gmail.com>
2022-08-22 20:55:43 +0700

    assignment 3 2022/08/22

--- assignmentfunction.go
+++ assignmentfunction.go
@@ -6,6 +6,11 @@ func main() {
 	names := []string{"zayan", "billy", "rizky", "fajar", "zaka", "hendar", "wahyu", "tambunan", "ramadhan", "utomo"}
 
 	for _, name := range names {
-		fmt.Println(name)
+		cetak(name)
 	}
+
+}
+
+func cetak(name string) {
+	fmt.Println(name)
 }
