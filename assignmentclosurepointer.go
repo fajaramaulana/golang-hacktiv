76705ff3e470832eccd9136e4c9bdd17c060dfac
fajaramaulana <fajaragusmaulana@gmail.com>
2022-08-24 19:42:23 +0700

    closure + pointer + struct

--- assignmentclosurepointer.go
+++ assignmentclosurepointer.go
@@ -2,30 +2,29 @@ package main
 
 import "fmt"
 
+type Manusia struct {
+	nama string
+}
+
 func main() {
-	zayan := "zayan"
-	billy := "billy"
-	rizky := "rizky"
-	fajar := "fajar"
-	zaka := "zaka"
-	hendar := "hendar"
-	wahyu := "wahyu"
-	tambunan := "tambunan"
-	ramadhan := "ramadhan"
-	utomo := "utomo"
 
-	names := []*string{&zayan, &billy, &rizky, &fajar, &zaka, &hendar, &wahyu, &tambunan, &ramadhan, &utomo}
+	names := []*Manusia{{nama: "fajar"}, {nama: "zaka"}, {nama: "hendar"}, {nama: "wahyu"}, {nama: "tambunan"}, {nama: "ramadhan"}, {nama: "utomo"}}
 
-	cetaknih := func(list []*string) []string {
+	cetaknih := func(list []*Manusia) []string {
+		// var result []string
+		// for _, name := range list {
+		// 	result = append(result, *name)
+		// }
+
+		// return result
+
 		var result []string
 		for _, name := range list {
-			result = append(result, *name)
+			result = append(result, name.nama)
 		}
 
 		return result
 	}
 
-	cetaknihye := cetaknih(names)
-
-	fmt.Println(cetaknihye)
+	fmt.Println(cetaknih(names))
 }
