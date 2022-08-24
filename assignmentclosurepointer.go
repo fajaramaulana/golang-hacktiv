70e22e62164d829b5889d65e7912cc04203d64e6
fajaramaulana <fajaragusmaulana@gmail.com>
2022-08-24 19:50:41 +0700

    closure + pointer + struct

--- assignmentclosurepointer.go
+++ assignmentclosurepointer.go
@@ -7,7 +7,6 @@ type Manusia struct {
 }
 
 func main() {
-
 	names := []*Manusia{{nama: "fajar"}, {nama: "zaka"}, {nama: "hendar"}, {nama: "wahyu"}, {nama: "tambunan"}, {nama: "ramadhan"}, {nama: "utomo"}}
 
 	cetaknih := func(list []*Manusia) []string {
@@ -27,4 +26,29 @@ func main() {
 	}
 
 	fmt.Println(cetaknih(names))
+	// zayan := "zayan"
+	// billy := "billy"
+	// rizky := "rizky"
+	// fajar := "fajar"
+	// zaka := "zaka"
+	// hendar := "hendar"
+	// wahyu := "wahyu"
+	// tambunan := "tambunan"
+	// ramadhan := "ramadhan"
+	// utomo := "utomo"
+
+	// names := []*string{&zayan, &billy, &rizky, &fajar, &zaka, &hendar, &wahyu, &tambunan, &ramadhan, &utomo}
+
+	// cetaknih := func(list []*string) []string {
+	// 	var result []string
+	// 	for _, name := range list {
+	// 		result = append(result, *name)
+	// 	}
+
+	// 	return result
+	// }
+
+	// cetaknihye := cetaknih(names)
+
+	// fmt.Println(cetaknihye)
 }
